e7d523099de1d93b58508b5e4069a99442b562f7
sam <samleefretwell@gmail.com>
2021-10-02 09:14:16 +0100

    Adds user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  email: "sam@example.com",
+  username: "sam",
+  first_name: "Sam",
+  last_name: "Example",
+  password: "secret123"
+)
